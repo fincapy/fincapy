5cc02464983526ea7ccebc91eb7de2ec43c3d8b8
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-12 16:47:01 -0700

    xss protection

--- next.config.js
+++ next.config.js
@@ -7,4 +7,45 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; connect-src 'self'",
+  },
+];
+
+nextConfig.headers = async () => {
+  return [
+    {
+      source: '/:path*',
+      headers: securityHeaders,
+    },
+  ];
+};
+
 module.exports = withBundleAnalyzer(nextConfig);

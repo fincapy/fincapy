95a033a2ae5c6c3ae8fc9cc0050e53d944a45da2
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-15 15:03:15 -0700

    fix bug

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -19,14 +19,13 @@ export default async function Layout({ children }) {
   const redisAdapter = new RedisAdapter({ redisClient });
   const tenantRepository = new TenantRepository({ redisAdapter });
   const tenant = await tenantRepository.get({ tenantId });
-  console.log(tenant);
 
   if (!tenant) {
     return <div>Tenant not found</div>;
   }
   if (tenant.billingStatus === 'unpaid' || !tenant.billingStatus) {
     redirect(
-      `https://buy.stripe.com/test_6oEeVmgk19t71moeUU?prefilled_email=${encodeURIComponent(user.email)}`
+      `https://buy.stripe.com/test_6oEeVmgk19t71moeUU?prefilled_email=${encodeURIComponent(userEmail)}`
     );
   }
 
@@ -37,7 +36,6 @@ export default async function Layout({ children }) {
   if (tenant.billingStatus === 'payment_failed') {
     redirect('https://billing.stripe.com/p/login/test_7sI28i4mUcdG8Ok4gg');
   }
-  console.log('here?');
 
   if (!planId) {
     planId = 'initial';

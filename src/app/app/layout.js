49e9986c974860e8f7ed65d4dc8338da53fa670a
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-15 15:09:34 -0700

    troubleshoot

--- src/app/app/layout.js
+++ src/app/app/layout.js
@@ -19,6 +19,7 @@ export default async function Layout({ children }) {
   const redisAdapter = new RedisAdapter({ redisClient });
   const tenantRepository = new TenantRepository({ redisAdapter });
   const tenant = await tenantRepository.get({ tenantId });
+  console.log(JSON.stringify(tenant));
 
   if (!tenant) {
     return <div>Tenant not found</div>;

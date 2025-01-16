c9e3906d59739087bb68d81860c8a47ce2343e74
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-16 14:42:06 -0700

    try this

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="overscroll-none" suppressHydrationWarning>
       <UserProvider>
         <body className="touch-none overscroll-none">{children}</body>
       </UserProvider>

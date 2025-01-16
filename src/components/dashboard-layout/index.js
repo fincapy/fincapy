91b1fcfb16b1d28f69db4c54534216dda5de2c71
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-16 14:34:10 -0700

    try this

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -337,7 +337,12 @@ export default function DashboardLayout({
         <EndDateContext.Provider value={{ endDateState, setEndDateState }}>
           <UsersContext.Provider value={{ usersState, setUsersState }}>
             <PageContext.Provider value={{ page, setPage }}>
-              <main className="w-full h-full overflow-hidden fixed inset-0 touch-none">
+              <main
+                className="w-full h-full overflow-hidden fixed inset-0 touch-none"
+                onScroll={(e) => {
+                  e.preventDefault();
+                }}
+              >
                 <ScrollArea
                   className="h-[94%] w-screen fixed top-0"
                   ref={scrollAreaRef}

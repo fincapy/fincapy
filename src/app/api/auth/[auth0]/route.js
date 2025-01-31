375621cbde64f1e315a33772fd656b71ced58789
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-31 15:15:44 -0700

    try this

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,10 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  session: {
+    cookie: {
+      sameSite: 'none', // Required for cross-subdomain auth
+      secure: process.env.NODE_ENV === 'production', // Enforce HTTPS in production
+    },
+  },
+});

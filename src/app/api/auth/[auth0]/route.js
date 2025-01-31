107efc5308dfcfa5ecf32d122d590013a6f8075d
Ryan Wible <ryanwiblecoding@gmail.com>
2025-01-31 15:31:47 -0700

    remove custom config

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,10 +1,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth({
-  session: {
-    cookie: {
-      sameSite: 'none', // Required for cross-subdomain auth
-      secure: process.env.NODE_ENV === 'production', // Enforce HTTPS in production
-    },
-  },
-});
+export const GET = handleAuth();

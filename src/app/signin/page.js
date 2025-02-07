e71da5258ca4e7385fdd3e411924a5b7e6c47aed
Ryan Wible <ryanwiblecoding@gmail.com>
2025-02-07 07:04:00 -0700

    add nonce

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,30 +1,9 @@
-'use client';
-
-import { GalleryVerticalEnd } from 'lucide-react';
 import { SignInForm } from '@/components/sign-in-form';
-import { ThemeProvider } from '@/components/theme-provider';
+import { headers } from 'next/headers';
+
 export default function SignInPage() {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <a
-            href="#"
-            className="flex items-center gap-2 self-center font-medium"
-          >
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Fincapy
-          </a>
-          <SignInForm />
-        </div>
-      </div>
-    </ThemeProvider>
-  );
+  const headersList = headers();
+  const nonce = headersList.get('x-nonce');
+
+  return <SignInForm nonce={nonce} />;
 }
--- src/components/sign-in-form/index.js
+++ src/components/sign-in-form/index.js
@@ -1,6 +1,5 @@
 'use client';
 
-import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -15,8 +14,10 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { authenticateEmailPassword } from './serverActions';
 import { Alert, AlertDescription } from '@/components/ui/alert';
+import { ThemeProvider } from '@/components/theme-provider';
+import { GalleryVerticalEnd } from 'lucide-react';
 
-export function SignInForm({ className, ...props }) {
+export function SignInForm({ nonce }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -52,81 +53,103 @@ export function SignInForm({ className, ...props }) {
   };
 
   return (
-    <div className={cn('flex flex-col gap-6', className)} {...props}>
-      <Card className="bg-background">
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back!</CardTitle>
-          <CardDescription>Sign in with your passkey</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
-                  Sign in with passkey
-                </Button>
-              </div>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-              <div className="grid gap-6">
-                <div className="grid gap-2 bg-background">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                    className="bg-background"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                {error && (
-                  <Alert variant="destructive">
-                    <AlertDescription>
-                      Invalid email or password
-                    </AlertDescription>
-                  </Alert>
-                )}
-                <Button type="submit" className="w-full">
-                  Sign in
-                </Button>
-              </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{' '}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+      nonce={nonce}
+    >
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
             </div>
-          </form>
-        </CardContent>
-      </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
-        and <a href="#">Privacy Policy</a>.
+            Fincapy
+          </a>
+          <div className="flex flex-col gap-6">
+            <Card className="bg-background">
+              <CardHeader className="text-center">
+                <CardTitle className="text-xl">Welcome back!</CardTitle>
+                <CardDescription>Sign in with your passkey</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={handleSubmit}>
+                  <div className="grid gap-6">
+                    <div className="flex flex-col gap-4">
+                      <Button variant="outline" className="w-full">
+                        Sign in with passkey
+                      </Button>
+                    </div>
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+                    <div className="grid gap-6">
+                      <div className="grid gap-2 bg-background">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                          id="email"
+                          type="email"
+                          placeholder="m@example.com"
+                          required
+                          className="bg-background"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </div>
+                      <div className="grid gap-2">
+                        <div className="flex items-center">
+                          <Label htmlFor="password">Password</Label>
+                          <a
+                            href="#"
+                            className="ml-auto text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </a>
+                        </div>
+                        <Input
+                          id="password"
+                          type="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                        />
+                      </div>
+                      {error && (
+                        <Alert variant="destructive">
+                          <AlertDescription>
+                            Invalid email or password
+                          </AlertDescription>
+                        </Alert>
+                      )}
+                      <Button type="submit" className="w-full">
+                        Sign in
+                      </Button>
+                    </div>
+                    <div className="text-center text-sm">
+                      Don&apos;t have an account?{' '}
+                      <a href="#" className="underline underline-offset-4">
+                        Sign up
+                      </a>
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+            </Card>
+            <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+              By clicking continue, you agree to our{' '}
+              <a href="#">Terms of Service</a> and{' '}
+              <a href="#">Privacy Policy</a>.
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }

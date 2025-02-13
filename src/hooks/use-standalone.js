e5f0bb5227b9aad635413f2701990837e924738f
Ryan Wible <ryanwiblecoding@gmail.com>
2025-02-13 07:30:17 -0700

    debug standalone

--- src/hooks/use-standalone.js
+++ src/hooks/use-standalone.js
@@ -1,9 +1,33 @@
 'use client';
+import { useEffect, useState } from 'react';
+
+function logToScreen(message) {
+  const el = document.createElement('div');
+  el.textContent = message;
+  el.style.position = 'fixed';
+  el.style.top = '0';
+  el.style.left = '0';
+  el.style.backgroundColor = 'rgba(0,0,0,0.7)';
+  el.style.color = 'white';
+  el.style.padding = '10px';
+  el.style.zIndex = '9999';
+  document.body.appendChild(el);
+}
 
 export function useStandalone() {
-  if (typeof window !== 'undefined') {
-    return window.navigator.standalone || 
-           window.matchMedia('(display-mode: standalone)').matches;
-  }
-  return false;
+  const [isStandalone, setIsStandalone] = useState(false);
+
+  logToScreen(`Standalone: ${window.navigator.standalone}`);
+  logToScreen(
+    `Display Mode: ${window.matchMedia('(display-mode: standalone)').matches}`
+  );
+
+  useEffect(() => {
+    const standalone =
+      window.matchMedia('(display-mode: standalone)').matches ||
+      window.navigator.standalone === true;
+    setIsStandalone(standalone);
+  }, []);
+
+  return isStandalone;
 }

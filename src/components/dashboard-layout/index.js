394c561f5b8d87839df2150e5ad55aa398bce43e
Ryan Wible <ryanwiblecoding@gmail.com>
2025-02-12 22:01:17 -0700

    resize

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -174,7 +174,7 @@ const NavBar = ({
 
   return (
     <div
-      className={`flex flex-col items-center justify-center bg-background z-20 w-full h-[6%] fixed bottom-0 m-0 touch-none ${isStandalone ? 'pb-5' : ''}`}
+      className={`flex flex-col items-center justify-center bg-background z-20 w-full h-[6%] fixed bottom-0 m-0 touch-none ${isStandalone ? 'pb-8 h-[10%]' : ''}`}
     >
       <Separator className="w-full h-[1px]" />
       <div className="flex flex-row justify-center items-center lg:w-[33.33%] md:w-[50%] w-11/12 h-full">
@@ -387,6 +387,8 @@ export default function DashboardLayout({
     }
   };
 
+  const isStandalone = useStandalone();
+
   return (
     <ThemeProvider
       attribute="class"
@@ -404,7 +406,7 @@ export default function DashboardLayout({
               onMouseDown={handleScrollAreaFocus}
             >
               <ScrollAreaWithPulldown
-                className="h-[94%] w-screen fixed top-0"
+                className={`h-[94%] ${isStandalone && 'h-[90%]'} w-screen fixed top-0`}
                 ref={scrollAreaRef}
                 onTouchStart={handleScrollAreaFocus}
                 triggerRefresh={() => {

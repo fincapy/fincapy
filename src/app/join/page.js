c9f0fe3cc12e758076ab6e0211dc13b255d72849
Ryan Wible <ryanwiblecoding@gmail.com>
2025-02-20 08:28:59 -0700

    feat: Create join page component for user registration

---
+++ src/app/join/page.js
@@ -0,0 +1,46 @@
+import { SignInForm } from '@/components/sign-in-form';
+import { headers, cookies } from 'next/headers';
+import { SessionManager } from '@/backend/adapters/auth';
+import { SessionRepository } from '@/backend/adapters/repositories/sessionRepository';
+import { RedisAdapter, redisClient } from '@/backend/adapters/redisAdapter';
+import { redirect } from 'next/navigation';
+import { GalleryVerticalEnd } from 'lucide-react';
+import { ThemeProvider } from '@/components/theme-provider';
+import jwt from 'jsonwebtoken';
+
+export default async function JoinPage({ params }) {
+  const headersList = headers();
+  const nonce = headersList.get('x-nonce');
+  const token = params.token;
+  let verifiedToken;
+  try {
+    verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return 'Unauthorized';
+  }
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+      nonce={nonce}
+    >
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Fincapy
+          </a>
+          <SignInForm mfaToken={mfaToken} />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}

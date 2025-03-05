3342cc0c4b772ca4fd4b2f0f92e55b8a001856c0
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-05 07:33:56 -0700

    fix plaid execution

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ export async function middleware(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
   const prodCspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://cdn.plaid.com;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://plaid.com https://*.plaid.com;
     connect-src 'self' https://*.plaid.com;
     frame-src 'self' https://plaid.com https://*.plaid.com;
     child-src 'self' https://plaid.com https://*.plaid.com;

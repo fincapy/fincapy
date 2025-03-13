4b910f56ea181edeb6185000b0f78e0c5a603424
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-13 10:25:38 -0600

    fix headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,23 @@ const bundleAnalyzerConfig = {
 
 const nextConfig = {
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withBundleAnalyzer(bundleAnalyzerConfig)(nextConfig);

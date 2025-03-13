10659c5975681cc5be8e83674b5508e18de2e7d6
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-13 08:23:18 -0600

    fix csp

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ export async function middleware(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
   const prodCspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' strict-dynamic;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     connect-src 'self' https://*.plaid.com;
     frame-src 'self' https://plaid.com https://*.plaid.com;
     child-src 'self' https://plaid.com https://*.plaid.com;

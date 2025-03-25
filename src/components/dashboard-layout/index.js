5f1a23b5b0fcc0541d0507a5247000fffc6f5c61
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-25 09:21:59 -0600

    try this?

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -424,7 +424,7 @@ export default function DashboardLayout({
       <EndDateContext.Provider value={{ endDateState, setEndDateState }}>
         <PageContext.Provider value={{ page, setPage }}>
           <main
-            className="w-full h-full overflow-hidden fixed inset-0 touch-none pt-safe pl-safe pr-safe"
+            className="w-full h-full overflow-hidden fixed inset-0 touch-none pt-safe pb-safe pl-safe pr-safe"
             onTouchStart={handleScrollAreaFocus}
             onMouseDown={handleScrollAreaFocus}
           >

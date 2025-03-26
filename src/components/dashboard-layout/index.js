4b76e649786ce80ea665c652801d16e77350fd20
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-26 09:51:16 -0600

    test

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -423,7 +423,7 @@ export default function DashboardLayout({
     <StartDateContext.Provider value={{ startDateState, setStartDateState }}>
       <EndDateContext.Provider value={{ endDateState, setEndDateState }}>
         <PageContext.Provider value={{ page, setPage }}>
-          <div className="absolute bottom-0 h-[env(safe-area-inset-bottom)] w-full bg-primarys z-50" />
+          <div className="absolute bottom-0 h-[env(safe-area-inset-bottom)] w-screen bg-primary z-50" />
           <main
             className="w-full h-[100vh] overflow-hidden fixed inset-0 touch-none pt-safe pl-safe pr-safe pb-safe"
             onTouchStart={handleScrollAreaFocus}

23995ee16088d4163fc47e461d9b3b392047aab1
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-26 08:24:36 -0600

    try this

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -183,7 +183,7 @@ const NavBar = ({
 
   return (
     <div
-      className={`flex flex-col items-center justify-center bg-card z-20 w-full fixed bottom-0 m-0 touch-none h-[calc(6%+env(safe-area-inset-bottom))] pb-safe`}
+      className={`flex flex-col items-center justify-center bg-card z-20 w-full fixed bottom-0 m-0 touch-none h-[6%]`}
     >
       <Separator className="w-full h-[1px] bg-border" />
       <div className="flex flex-row justify-center items-center lg:w-[33.33%] md:w-[50%] w-11/12 h-full">

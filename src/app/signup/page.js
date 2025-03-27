105e839487c0575f5c3f4cea70a92a1567361608
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-26 21:44:15 -0600

    background color

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -10,7 +10,7 @@ export default async function SignUpPage() {
 
   return (
     <AccessGate>
-      <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 bg-background">
         <div className="flex w-full max-w-sm flex-col gap-6">
           <a
             href="#"

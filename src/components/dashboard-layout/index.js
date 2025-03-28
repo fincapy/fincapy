119a936167434e5c81d7e141b5dbeb97e4224f3a
Ryan Wible <ryanwiblecoding@gmail.com>
2025-03-28 14:59:16 -0600

    fix separator

--- src/components/dashboard-layout/index.js
+++ src/components/dashboard-layout/index.js
@@ -185,7 +185,7 @@ const NavBar = ({
     <div
       className={`flex flex-col items-center justify-center bg-card z-20 w-full fixed bottom-0 m-0 touch-none h-[6%] select-none`}
     >
-      <Separator className="w-full h-[0.6px] bg-border" />
+      <Separator className="w-full h-[1px] bg-border" />
       <div className="flex flex-row justify-center items-center lg:w-[33.33%] md:w-[50%] w-11/12 h-full">
         <div className="flex flex-row justify-between items-center flex-1">
           <button

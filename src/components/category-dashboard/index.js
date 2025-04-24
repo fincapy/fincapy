8f95ecee9cc841698f076e91b26eed2989023762
Ryan Wible <ryanwiblecoding@gmail.com>
2025-04-24 12:33:56 -0600

    fix currency

--- src/components/category-dashboard/index.js
+++ src/components/category-dashboard/index.js
@@ -1504,11 +1504,15 @@ const CategoryCard = ({
                 {`${new Intl.NumberFormat('en-US', {
                   style: 'currency',
                   currency: 'USD',
+                  maximumFractionDigits: 0,
+                  minimumFractionDigits: 0,
                 }).format(category.currentNet)} / ${new Intl.NumberFormat(
                   'en-US',
                   {
                     style: 'currency',
                     currency: 'USD',
+                    maximumFractionDigits: 0,
+                    minimumFractionDigits: 0,
                   }
                 ).format(category.proratedGoal)}`}
               </span>

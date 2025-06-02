08e2909cd5a34234f1bc44facd807fa2be59cc73
Ryan Wible <ryanwiblecoding@gmail.com>
2025-06-01 19:38:27 -0600

    fix

--- src/components/transaction-table/index.jsx
+++ src/components/transaction-table/index.jsx
@@ -1,16 +1,7 @@
 import { columns } from './columns';
 import { DataTable } from './data-table';
-import { VirtualizedDataTable } from './virtualized-data-table';
 
 const TransactionTable = ({ transactions }) => {
-  // Use virtualization for large datasets (>100 transactions)
-  // This provides significant performance improvements for large tables
-  // const useVirtualization = transactions && transactions.length > 5;
-
-  // if (useVirtualization) {
-  //   return <VirtualizedDataTable columns={columns} data={transactions} />;
-  // }
-
   return <DataTable columns={columns} data={transactions} />;
 };
 
